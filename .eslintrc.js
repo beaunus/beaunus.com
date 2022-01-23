a32aeda7f324fda63ae2eb2d26a6b7c4c5c6c342
Beau Dobbin <beau@beaunus.com>
2022-01-23 14:32:53 +0900

    chore: install @beaunus123/style-config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const beaunusStyleConfig = require("@beaunus123/style-config").eslint;
+
+module.exports = {
+  ...beaunusStyleConfig,
+  extends: [
+    ...beaunusStyleConfig.extends,
+    "next/core-web-vitals",
+    "plugin:tailwindcss/recommended",
+  ],
+};
