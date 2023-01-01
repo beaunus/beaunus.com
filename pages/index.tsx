562ceb7b25decfce96c9977692988e15574ffbba
Beau Dobbin <beau@beaunus.com>
2023-01-01 09:57:44 +0900

    refactor: extract index sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,76 @@ import { JobsSection } from "../components/JobsSection";
 import * as Logos from "../images/logos";
 import * as Photos from "../images/photos";
 
+const Header: React.FC = () => (
+  <header className="flex flex-row-reverse p-3 mb-8">
+    <Image
+      alt="Beaunus Logo"
+      height={25}
+      priority={true}
+      src={Logos.BeaunusPixels}
+      width={119.25}
+    />
+  </header>
+);
+
+const TitleSection: React.FC = () => (
+  <div className="flex flex-col grow shrink-0 gap-5 items-center px-3">
+    <div>
+      <Image
+        alt="Beau Dobbin"
+        className="rounded-full"
+        height={200}
+        priority={true}
+        src={Photos.BeauDobbinPhoto}
+        width={200}
+      />
+    </div>
+    <div>
+      <div className="text-4xl font-semibold">Beau Dobbin</div>
+      <div className="text-2xl font-semibold text-cyan-700">
+        Software Engineer
+      </div>
+    </div>
+    <div className="flex flex-wrap gap-5 justify-center">
+      <div className="flex gap-2 items-center">
+        <FontAwesomeIcon className="text-blue-700" icon={faTerminal} />
+        <div>Code</div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <FontAwesomeIcon className="text-green-800" icon={faHandshake} />
+        <div>Education</div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <FontAwesomeIcon className="text-amber-500" icon={faMusic} />
+        <div>Music</div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <FontAwesomeIcon className="text-red-600" icon={faHeadphones} />
+        <div>Audio Engineering</div>
+      </div>
+    </div>
+  </div>
+);
+
+const Footer: React.FC = () => (
+  <footer className="flex flex-col shrink-0 gap-8 justify-evenly items-center py-16 px-5 odd:mt-2 odd:bg-gray-100">
+    <FontAwesomeIcon icon={faHandPeace} />
+    <HighlightedLink href="mailto:beau@beaunus.com" label="beau@beaunus.com" />
+    <div className="flex flex-wrap gap-8 justify-center">
+      <HighlightedLink
+        href="https://github.com/beaunus"
+        label={<Image alt="Github" height={24} src={Logos.Github} width={89} />}
+      />
+      <HighlightedLink
+        href="https://www.linkedin.com/in/beaunus/"
+        label={
+          <Image alt="Linkedin" height={24} src={Logos.LinkedIn} width={91} />
+        }
+      />
+    </div>
+  </footer>
+);
+
 const Home: NextPage = () => (
   <>
     <Head>
@@ -26,79 +96,11 @@ const Home: NextPage = () => (
       />
       <meta content="width=device-width,initial-scale=1" name="viewport" />
     </Head>
+    <Header />
     <div className="flex flex-col gap-2 text-center">
-      <header className="flex flex-row-reverse p-3 mb-8">
-        <Image
-          alt="Beaunus Logo"
-          height={25}
-          priority={true}
-          src={Logos.BeaunusPixels}
-          width={119.25}
-        />
-      </header>
-      <div className="flex flex-col grow shrink-0 gap-5 items-center px-3">
-        <div>
-          <Image
-            alt="Beau Dobbin"
-            className="rounded-full"
-            height={200}
-            priority={true}
-            src={Photos.BeauDobbinPhoto}
-            width={200}
-          />
-        </div>
-        <div>
-          <div className="text-4xl font-semibold">Beau Dobbin</div>
-          <div className="text-2xl font-semibold text-cyan-700">
-            Software Engineer
-          </div>
-        </div>
-        <div className="flex flex-wrap gap-5 justify-center">
-          <div className="flex gap-2 items-center">
-            <FontAwesomeIcon className="text-blue-700" icon={faTerminal} />
-            <div>Code</div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <FontAwesomeIcon className="text-green-800" icon={faHandshake} />
-            <div>Education</div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <FontAwesomeIcon className="text-amber-500" icon={faMusic} />
-            <div>Music</div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <FontAwesomeIcon className="text-red-600" icon={faHeadphones} />
-            <div>Audio Engineering</div>
-          </div>
-        </div>
-      </div>
+      <TitleSection />
       <JobsSection />
-      <footer className="flex flex-col shrink-0 gap-8 justify-evenly items-center py-16 px-5 odd:mt-2 odd:bg-gray-100">
-        <FontAwesomeIcon icon={faHandPeace} />
-        <HighlightedLink
-          href="mailto:beau@beaunus.com"
-          label="beau@beaunus.com"
-        />
-        <div className="flex flex-wrap gap-8 justify-center">
-          <HighlightedLink
-            href="https://github.com/beaunus"
-            label={
-              <Image alt="Github" height={24} src={Logos.Github} width={89} />
-            }
-          />
-          <HighlightedLink
-            href="https://www.linkedin.com/in/beaunus/"
-            label={
-              <Image
-                alt="Linkedin"
-                height={24}
-                src={Logos.LinkedIn}
-                width={91}
-              />
-            }
-          />
-        </div>
-      </footer>
+      <Footer />
     </div>
   </>
 );
