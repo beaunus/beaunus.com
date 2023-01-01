cd864a8297adbdfbb89d6ce2c0d99980adb036f6
Beau Dobbin <beau@beaunus.com>
2023-01-01 09:49:49 +0900

    refactor: use Link instead of a

--- components/HighlightedLink.tsx
+++ components/HighlightedLink.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import React, { FC, ReactNode } from "react";
 
 export const HighlightedLink: FC<{ href: string; label: ReactNode }> = ({
   href,
   label,
 }) => (
-  <a
+  <Link
     className="hover:bg-cyan-100 border-b-2 border-b-cyan-100 transition duration-75 ease-in-out"
     href={href}
   >
     {label}
-  </a>
+  </Link>
 );
