b4eaf3fafd9a16fec02651854b04fdd5e42cda7d
Beau Dobbin <beau@beaunus.com>
2023-01-20 13:15:35 +0900

    feature: Spock

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { faHandPeace } from "@fortawesome/free-solid-svg-icons";
+import { faHandSpock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import React from "react";
@@ -8,7 +8,7 @@ import * as Logos from "../images/logos";
 
 export const Footer: React.FC = () => (
   <footer className="flex flex-col shrink-0 gap-8 justify-evenly items-center py-16 px-5 mt-2 w-full bg-gray-100">
-    <FontAwesomeIcon color="rgb(232,25,140)" icon={faHandPeace} />
+    <FontAwesomeIcon color="rgb(232,25,140)" icon={faHandSpock} />
     <HighlightedLink href="mailto:beau@beaunus.com" label="beau@beaunus.com" />
     <div className="flex flex-wrap gap-8 justify-center">
       <HighlightedLink
