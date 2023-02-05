023a260925ba20b94d50ac5fc08b30b20944e373
Beau Dobbin <beau@beaunus.com>
2023-02-05 15:49:27 +0900

    feature: improve padding

--- components/Segment.tsx
+++ components/Segment.tsx
@@ -4,7 +4,7 @@ export const Segment: FC<{ children?: ReactNode; image?: ReactNode }> = ({
   image,
   children,
 }) => (
-  <section className="flex flex-col gap-6 justify-evenly items-center py-16 px-5 even:mt-2 w-full even:bg-gray-100">
+  <section className="flex flex-col gap-6 justify-evenly items-center py-16 px-10 even:mt-2 w-full even:bg-gray-100">
     <div className="flex flex-col gap-5 items-center w-full md:flex-row md:gap-10 md:justify-center">
       {image}
       <div className="flex flex-col gap-5 items-center w-full max-w-2xl">
