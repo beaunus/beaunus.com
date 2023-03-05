d8270a236dbf0a7eff7eb4c7004f1c1c778187f3
Beau Dobbin <beau@beaunus.com>
2023-03-05 14:41:20 +0900

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import {
   faMusic,
   faTerminal,
 } from "@fortawesome/free-solid-svg-icons";
+import FS from "@isomorphic-git/lightning-fs";
+import { Button } from "@mui/material";
+import { globby } from "globby";
+import micromatch from "micromatch";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -42,12 +46,99 @@ const TitleSection: React.FC = () => (
   </Segment>
 );
 
+const GitThing: React.FC = () => {
+  const [state, setState] = React.useState<"idle" | "loading" | "loaded">(
+    "idle"
+  );
+
+  const fs = new FS("testfs2");
+  const pfs = fs.promises;
+
+  const thing = async () => {
+    // console.log(await pfs.readdir(myDir));
+    const dir = "/rabbit";
+    await pfs
+      .mkdir(dir)
+      .then((happy) => {
+        console.log({ happy });
+      })
+      .catch((error) => {
+        console.log({ error });
+      });
+
+    console.log(await pfs.readlink(dir));
+
+    // Now it should not be empty...
+    await pfs.readdir(dir);
+  };
+
+  React.useEffect(() => {
+    thing();
+  }, []);
+
+  return (
+    <>
+      {state}
+      <Button component="label" variant="contained">
+        Upload
+        <input
+          hidden
+          onChange={async (e) => {
+            setState("loading");
+            console.log({ e });
+            console.log(e.target);
+            const filesArray = Array.from(e.target.files || []);
+            const gitIgnore = filesArray.find(
+              (file) => file.name === ".gitignore"
+            );
+
+            const globsToIgnore =
+              (await gitIgnore
+                ?.text()
+                .then((fileString) =>
+                  fileString
+                    .split("\n")
+                    .filter((line) => line && !line.startsWith("#"))
+                )) ?? [];
+
+            const isMatch = micromatch.matcher("*", { ignore: globsToIgnore });
+
+            const filesNotIgnored = filesArray.filter(({ name }) =>
+              isMatch(name)
+            );
+            console.log({ filesArray, filesNotIgnored, gitIgnore });
+
+            setState("loaded");
+
+            for (let i = 0; i < filesNotIgnored.slice(0, 10).length; ++i) {
+              const file = filesNotIgnored[i];
+              await pfs
+                .writeFile(file.webkitRelativePath, await file.text())
+                .catch((e) => console.log({ e }));
+            }
+
+            console.log(await pfs.du("."));
+          }}
+          onClick={() => {
+            setState("loading");
+          }}
+          onLoadedData={(x) => console.log({ x })}
+          type="file"
+          // @ts-expect-error Property 'webkitdirectory' does not exist on type 'DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>'.
+          webkitdirectory="true"
+        />
+      </Button>
+    </>
+  );
+};
+
 const Home: NextPage = () => (
   <>
     <Head>
       <title>Beaunus</title>
     </Head>
     <div className="flex flex-col grow gap-20 items-center text-center">
+      <GitThing />
       <TitleSection />
       <JobsSection />
     </div>
