26b89d99810ba6ddb7a96c8eaff756587208a12c
Beau Dobbin <beau@beaunus.com>
2023-03-15 21:16:10 +0900

    feature: add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { Nunito } from "@next/font/google";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import React from "react";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -17,6 +18,21 @@ const nunito = Nunito({ subsets: ["latin"] });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        dangerouslySetInnerHTML={{
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {page_path: window.location.pathname, });`,
+        }}
+        id="gtag"
+      />
+      {/* Google Tag Manager */}
+      <Script
+        dangerouslySetInnerHTML={{
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          __html: `window.dataLayer = window.dataLayer || [];(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`,
+        }}
+        id="gtm-script"
+      />
       <Head>
         <meta content="width=device-width,initial-scale=1" name="viewport" />
       </Head>
