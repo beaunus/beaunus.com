6154df47acc2f0468ea65c398ad013c12ad2d101
Beau Dobbin <beau@beaunus.com>
2023-03-26 11:03:35 +1000

    refactor: extract CommitTable

--- pages/blog/git-thing.tsx
+++ pages/blog/git-thing.tsx
@@ -1,5 +1,3 @@
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import { TextField, Typography } from "@mui/material";
@@ -12,24 +10,17 @@ import Grid from "@mui/material/Grid";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import Switch from "@mui/material/Switch";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import ChartJS from "chart.js/auto";
 import encHex from "crypto-js/enc-hex";
 import sha256 from "crypto-js/sha256";
 import dayjs, { Dayjs } from "dayjs";
-import Linkify from "linkify-react";
 import _ from "lodash";
 import multimatch from "multimatch";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { FC, useEffect, useRef, useState } from "react";
 
-import { HighlightedLink } from "../../components/HighlightedLink";
+import { CommitTable } from "../../components/git-thing/CommitTable";
 import { SliderWithLabels } from "../../components/SliderWithLabels";
 import {
   computeDateRange,
@@ -265,147 +256,6 @@ const GitThing: NextPage = () => {
       />
     </Button>
   );
-
-  const CommitTable: FC<{ commits: GitCommit[]; relevantFilePath: string }> = ({
-    commits,
-    relevantFilePath,
-  }) => {
-    const HEADINGS = [
-      "commitHash",
-      "date",
-      "author",
-      "+",
-      "-",
-      "-/+",
-      "abs(-/+)",
-      "message",
-    ] as const;
-
-    const [sortStrategy, setSortStrategy] = useState<{
-      criteria: (typeof HEADINGS)[number];
-      direction: "asc" | "desc";
-    }>({ criteria: "date", direction: "asc" });
-
-    const sortedCommits = _.sortBy(
-      commits.map((commit) => {
-        const relevantFile = commit.files.find(
-          ({ path }) => path.afterChange === relevantFilePath
-        );
-        return {
-          ...commit,
-          numLinesAddedForRelevantFile: relevantFile?.numLinesAdded ?? 0,
-          numLinesChangedForRelevantFile:
-            (relevantFile?.numLinesAdded ?? 0) -
-            (relevantFile?.numLinesDeleted ?? 0),
-          numLinesDeletedForRelevantFile: relevantFile?.numLinesDeleted ?? 0,
-          numLinesDiffForRelevantFile:
-            (relevantFile?.numLinesAdded ?? 0) +
-            (relevantFile?.numLinesDeleted ?? 0),
-        };
-      }),
-      sortStrategy.criteria === "+"
-        ? "numLinesAddedForRelevantFile"
-        : sortStrategy.criteria === "-"
-        ? "numLinesDeletedForRelevantFile"
-        : sortStrategy.criteria === "-/+"
-        ? "numLinesChangesForRelevantFile"
-        : sortStrategy.criteria === "abs(-/+)"
-        ? "numLinesDiffForRelevantFile"
-        : sortStrategy.criteria
-    );
-
-    return (
-      <TableContainer>
-        <Table aria-label="commit table" size="small">
-          <TableHead>
-            <TableRow className="whitespace-nowrap">
-              {HEADINGS.map((heading) => (
-                <TableCell
-                  key={heading}
-                  onClick={() => {
-                    setSortStrategy((old) => ({
-                      criteria: heading,
-                      direction:
-                        old.criteria === heading
-                          ? old.direction === "asc"
-                            ? "desc"
-                            : "asc"
-                          : "asc",
-                    }));
-                  }}
-                >
-                  {heading}
-                  {sortStrategy.direction === "asc" ? (
-                    <ArrowDropUpIcon
-                      color={
-                        sortStrategy.criteria === heading
-                          ? "inherit"
-                          : "disabled"
-                      }
-                    />
-                  ) : (
-                    <ArrowDropDownIcon
-                      color={
-                        sortStrategy.criteria === heading
-                          ? "inherit"
-                          : "disabled"
-                      }
-                    />
-                  )}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(sortStrategy.direction === "asc"
-              ? sortedCommits
-              : sortedCommits.reverse()
-            ).map((commit) => (
-              <TableRow
-                key={commit.commitHash}
-                sx={{
-                  // eslint-disable-next-line @typescript-eslint/naming-convention
-                  "&:last-child td, &:last-child th": { border: 0 },
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  <HighlightedLink
-                    href={`https://github.com/${baseGithubRepository}/commit/${commit.commitHash}`}
-                  >
-                    <code>{commit.commitHash.slice(0, 7)}</code>
-                  </HighlightedLink>
-                </TableCell>
-                <TableCell className="whitespace-nowrap">
-                  {dayjs(commit.date).format("YYYY-MM-DD")}
-                </TableCell>
-                <TableCell className="whitespace-nowrap">
-                  {commit.author.split(" <")[0]}
-                </TableCell>
-                <TableCell>{commit.numLinesAddedForRelevantFile}</TableCell>
-                <TableCell>{commit.numLinesDeletedForRelevantFile}</TableCell>
-                <TableCell>{commit.numLinesChangedForRelevantFile}</TableCell>
-                <TableCell>{commit.numLinesDiffForRelevantFile}</TableCell>
-                <TableCell className="whitespace-pre-line">
-                  <Linkify
-                    options={{
-                      render: ({ attributes: { href, ...props }, content }) => (
-                        <HighlightedLink href={href} {...props}>
-                          {content}
-                        </HighlightedLink>
-                      ),
-                    }}
-                  >
-                    <strong>{commit.message.split("\n")[0]}</strong>
-                    {commit.message.split("\n").slice(1).join("\n")}
-                  </Linkify>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  };
 
   return (
     <>
@@ -698,6 +548,7 @@ const GitThing: NextPage = () => {
           <>
             <Typography variant="h5">{focusedDataEntry[0]}</Typography>
             <CommitTable
+              baseGithubRepository={baseGithubRepository}
               commits={focusedDataEntry[1].commits.filter(
                 ({ author }) =>
                   !authorsToInclude.length || authorsToInclude.includes(author)
