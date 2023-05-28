f7b102c26c3407c8eb033f533d932eceb7fe494d
Beau Dobbin <beau@beaunus.com>
2023-05-28 18:37:08 +0900

    refactor: move REPOSITORY_SUMMARY_METRICS out of component

--- components/git-thing/RepositorySummaryTable.tsx
+++ components/git-thing/RepositorySummaryTable.tsx
@@ -11,134 +11,146 @@ import { FC, ReactNode } from "react";
 
 import { GitCommit } from "../../utils/git";
 
-export const RepositorySummaryTable: FC<{
-	allCommits: GitCommit[];
-	allCommitsFiltered: GitCommit[];
-}> = ({ allCommits, allCommitsFiltered }) => {
-	const REPOSITORY_SUMMARY_METRICS: {
-		details: ReactNode;
-		label: ReactNode;
-		value: ReactNode;
-	}[] = [
-		{
-			details: (
-				<details>
-					<summary>Details</summary>
-					<ul>
-						{allCommits.map(({ message }, index) => (
-							<li key={`commits-all-time-${index}`}>{message}</li>
-						))}
-					</ul>
-				</details>
-			),
-			label: "Number of commits (all time)",
-			value: Number(allCommits.length).toLocaleString(),
-		},
-		{
-			details: (
-				<details>
-					<summary>Details</summary>
-					<ul>
-						{allCommitsFiltered.map(({ message }, index) => (
-							<li key={`commits-filtered-${index}`}>{message}</li>
-						))}
-					</ul>
-				</details>
-			),
-			label: "Number of commits (based on filters)",
-			value: Number(allCommitsFiltered.length).toLocaleString(),
-		},
-		{
-			details: null,
-			label: "% of all commits (based on filters)",
-			value: allCommits.length
+const REPOSITORY_SUMMARY_METRICS: {
+	label: ReactNode;
+	resolverDetails: (commits: {
+		allCommits: GitCommit[];
+		allCommitsFiltered: GitCommit[];
+	}) => ReactNode;
+	resolverValue: (commits: {
+		allCommits: GitCommit[];
+		allCommitsFiltered: GitCommit[];
+	}) => ReactNode;
+}[] = [
+	{
+		label: "Number of commits (all time)",
+		resolverDetails: ({ allCommits }) => (
+			<details>
+				<summary>Details</summary>
+				<ul>
+					{allCommits.map(({ message }, index) => (
+						<li key={`commits-all-time-${index}`}>{message}</li>
+					))}
+				</ul>
+			</details>
+		),
+		resolverValue: ({ allCommits }) =>
+			Number(allCommits.length).toLocaleString(),
+	},
+	{
+		label: "Number of commits (based on filters)",
+		resolverDetails: ({ allCommitsFiltered }) => (
+			<details>
+				<summary>Details</summary>
+				<ul>
+					{allCommitsFiltered.map(({ message }, index) => (
+						<li key={`commits-filtered-${index}`}>{message}</li>
+					))}
+				</ul>
+			</details>
+		),
+		resolverValue: ({ allCommitsFiltered }) =>
+			Number(allCommitsFiltered.length).toLocaleString(),
+	},
+	{
+		label: "% of all commits (based on filters)",
+		resolverDetails: () => null,
+		resolverValue: ({ allCommits, allCommitsFiltered }) =>
+			allCommits.length
 				? `${((100 * allCommitsFiltered.length) / allCommits.length).toFixed(
 						2
 				  )}%`
 				: null,
-		},
-		{
-			details: (
-				<details>
-					<summary>Details</summary>
-					<ul>
-						{_.uniqBy(allCommits, ({ author }) => author)
-							.sort((a, b) =>
-								a.author < b.author ? -1 : a.author > b.author ? 1 : 0
-							)
-							.map(({ author }) => (
-								<li key={`num-committers-${author}`}>{author}</li>
-							))}
-					</ul>
-				</details>
-			),
-			label: "Number of committers (all time)",
-			value: Number(
+	},
+	{
+		label: "Number of committers (all time)",
+		resolverDetails: ({ allCommits }) => (
+			<details>
+				<summary>Details</summary>
+				<ul>
+					{_.uniqBy(allCommits, ({ author }) => author)
+						.sort((a, b) =>
+							a.author < b.author ? -1 : a.author > b.author ? 1 : 0
+						)
+						.map(({ author }) => (
+							<li key={`num-committers-${author}`}>{author}</li>
+						))}
+				</ul>
+			</details>
+		),
+		resolverValue: ({ allCommits }) =>
+			Number(
 				_.uniqBy(allCommits, ({ author }) => author).length
 			).toLocaleString(),
-		},
-		{
-			details: (
-				<details>
-					<summary>Details</summary>
-					<ul>
-						{_.uniqBy(allCommitsFiltered, ({ author }) => author)
-							.sort((a, b) =>
-								a.author < b.author ? -1 : a.author > b.author ? 1 : 0
-							)
-							.map(({ author }) => (
-								<li key={`num-committers-${author}`}>{author}</li>
-							))}
-					</ul>
-				</details>
-			),
-			label: "Number of committers (based on filters)",
-			value: Number(
+	},
+	{
+		label: "Number of committers (based on filters)",
+		resolverDetails: ({ allCommitsFiltered }) => (
+			<details>
+				<summary>Details</summary>
+				<ul>
+					{_.uniqBy(allCommitsFiltered, ({ author }) => author)
+						.sort((a, b) =>
+							a.author < b.author ? -1 : a.author > b.author ? 1 : 0
+						)
+						.map(({ author }) => (
+							<li key={`num-committers-${author}`}>{author}</li>
+						))}
+				</ul>
+			</details>
+		),
+		resolverValue: ({ allCommitsFiltered }) =>
+			Number(
 				_.uniqBy(allCommitsFiltered, ({ author }) => author).length
 			).toLocaleString(),
-		},
-		{
-			details: null,
-			label: "% of all committers (based on filters)",
-			value: allCommits.length
+	},
+	{
+		label: "% of all committers (based on filters)",
+		resolverDetails: () => null,
+		resolverValue: ({ allCommits, allCommitsFiltered }) =>
+			allCommits.length
 				? `${(
 						(100 *
 							_.uniqBy(allCommitsFiltered, ({ author }) => author).length) /
 						_.uniqBy(allCommits, ({ author }) => author).length
 				  ).toFixed(2)}%`
 				: null,
-		},
-	];
+	},
+];
 
-	return (
-		<TableContainer>
-			<Table aria-label="criteria table" size="small">
-				<TableHead>
-					<TableRow className="whitespace-nowrap">
-						<TableCell component="th">Metric</TableCell>
-						<TableCell component="th">Value</TableCell>
-						<TableCell component="th" width="100%">
-							Details
-						</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{REPOSITORY_SUMMARY_METRICS.map(
-						({ label, details, value }, index) => (
-							<TableRow
-								className="align-top"
-								key={`repository-summary-metrics-${index}`}
-							>
-								<TableCell className="whitespace-nowrap">{label}</TableCell>
-								<TableCell align="right" className="font-mono">
-									{value}
-								</TableCell>
-								<TableCell>{details}</TableCell>
-							</TableRow>
-						)
-					)}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
-};
+export const RepositorySummaryTable: FC<{
+	allCommits: GitCommit[];
+	allCommitsFiltered: GitCommit[];
+}> = ({ allCommits, allCommitsFiltered }) => (
+	<TableContainer>
+		<Table aria-label="criteria table" size="small">
+			<TableHead>
+				<TableRow className="whitespace-nowrap">
+					<TableCell component="th">Metric</TableCell>
+					<TableCell component="th">Value</TableCell>
+					<TableCell component="th" width="100%">
+						Details
+					</TableCell>
+				</TableRow>
+			</TableHead>
+			<TableBody>
+				{REPOSITORY_SUMMARY_METRICS.map(
+					({ label, resolverDetails, resolverValue }, index) => (
+						<TableRow
+							className="align-top"
+							key={`repository-summary-metrics-${index}`}
+						>
+							<TableCell className="whitespace-nowrap">{label}</TableCell>
+							<TableCell align="right" className="font-mono">
+								{resolverValue({ allCommits, allCommitsFiltered })}
+							</TableCell>
+							<TableCell>
+								{resolverDetails({ allCommits, allCommitsFiltered })}
+							</TableCell>
+						</TableRow>
+					)
+				)}
+			</TableBody>
+		</Table>
+	</TableContainer>
+);
