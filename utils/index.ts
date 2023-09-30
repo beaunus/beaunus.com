309e2d26ec367c72fcae24febc1790cf5e092b94
Beau Dobbin <beau@beaunus.com>
2023-09-30 12:52:26 +0900

    refactor: use arrow functions

--- utils/index.ts
+++ utils/index.ts
@@ -24,27 +24,21 @@ export const isDateWithinSelectedRange = (
 export const moduloPositive = (dividend: number, divisor: number) =>
 	((dividend % divisor) + divisor) % divisor;
 
-export function sleep(ms: number) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
+export const sleep = (ms: number) =>
+	new Promise((resolve) => setTimeout(resolve, ms));
 
-export function stringToHex(string: string) {
-	return `#${sha256(string).toString(encHex).slice(0, 6)}`;
-}
+export const stringToHex = (string: string) =>
+	`#${sha256(string).toString(encHex).slice(0, 6)}`;
 
-export function stringToHsl(
+export const stringToHsl = (
 	string: string,
 	{ alpha, lightness, saturation } = {
 		alpha: 0.5,
 		lightness: 50,
 		saturation: 100,
 	}
-) {
-	return `hsl(${
-		stringToInt(string) % 360
-	}, ${saturation}%, ${lightness}%, ${alpha})`;
-}
+) =>
+	`hsl(${stringToInt(string) % 360}, ${saturation}%, ${lightness}%, ${alpha})`;
 
-export function stringToInt(string: string) {
-	return parseInt(sha256(string).toString(encHex), 16);
-}
+export const stringToInt = (string: string) =>
+	parseInt(sha256(string).toString(encHex), 16);
