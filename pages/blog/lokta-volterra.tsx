116482b589c010b50a60647a00af347c44586929
Beau Dobbin <beau@beaunus.com>
2023-10-01 15:05:41 +0900

    feature: mating

--- pages/blog/lokta-volterra.tsx
+++ pages/blog/lokta-volterra.tsx
@@ -1,4 +1,6 @@
 import ChartJS, { Point } from "chart.js/auto";
+import _ from "lodash";
+import { KDTree } from "mnemonist";
 import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
@@ -10,28 +12,54 @@ import {
 import { Segment } from "../../components/Segment";
 import { polarToCartesian } from "../../utils";
 
-type Fox = { lifespan: number; numTrialsSurvivedSoFar: number; point: Point };
+type Fox = {
+	id: string;
+	lifespan: number;
+	numTrialsSinceLastReproduction: number;
+	numTrialsSurvivedSoFar: number;
+	point: Point;
+};
 
-const MAXIMUM_LIFESPAN = 1000;
+const aFox = (point: Point = { x: Math.random(), y: Math.random() }): Fox => ({
+	id: _.uniqueId("fox"),
+	lifespan: Math.floor(MAX_LIFESPAN * (0.5 + Math.random() * 0.5)),
+	numTrialsSinceLastReproduction: 0,
+	numTrialsSurvivedSoFar: 100,
+	point,
+});
+
+const MAX_LIFESPAN = 1000;
+const MAX_MATING_DISTANCE = 0.017;
+const MIN_MATING_AGE = 100;
+const MATING_RECOVERY_DURATION = 90;
+const INITIAL_NUM_FOXES = 10;
+const STEP_SIZE = 0.05;
 
 const LoktaVolterra: NextPage = () => {
 	const scatterChartRef = React.useRef<HTMLCanvasElement>(null);
+	const lineChartRef = React.useRef<HTMLCanvasElement>(null);
 
 	const [foxes, setFoxes] = React.useState<Fox[]>([]);
+	const [numFoxesAfterEachTrial, setNumFoxesAfterEachTrial] = React.useState<
+		{ thatCanMate: number; total: number }[]
+	>([]);
 
 	React.useEffect(() => {
-		if (scatterChartRef.current) {
+		if (scatterChartRef.current && lineChartRef.current) {
 			const scatterChart = new ChartJS(scatterChartRef.current, {
 				data: {
 					datasets: [
 						{
-							backgroundColor: "#00f",
+							backgroundColor: ({ dataIndex }) =>
+								foxes[dataIndex] && canFoxReproduce(foxes[dataIndex])
+									? "#f00"
+									: "#00f",
 							borderColor: "#f00",
 							data: foxes.map(({ point }) => point),
 							pointRadius: ({ dataIndex }) =>
-								(MAXIMUM_LIFESPAN -
+								(MAX_LIFESPAN -
 									(foxes[dataIndex]?.numTrialsSurvivedSoFar ?? 0)) /
-								100,
+								(MAX_LIFESPAN / 10),
 						},
 						{
 							backgroundColor: "rgba(0,0,0,0)",
@@ -52,7 +80,10 @@ const LoktaVolterra: NextPage = () => {
 								footer: ([tooltipItem]) => {
 									const fox = foxes[tooltipItem.dataIndex];
 									return [
+										`index: ${tooltipItem.dataIndex}`,
+										`id: ${fox.id}`,
 										`lifespan: ${fox.lifespan}`,
+										`numTrialsSinceLastReproduction: ${fox.numTrialsSinceLastReproduction}`,
 										`numTrialsSurvivedSoFar: ${fox.numTrialsSurvivedSoFar}`,
 									].join("\n");
 								},
@@ -64,7 +95,28 @@ const LoktaVolterra: NextPage = () => {
 				type: "scatter",
 			});
 
+			const lineChart = new ChartJS(lineChartRef.current, {
+				data: {
+					datasets: [
+						{
+							backgroundColor: "none",
+							borderColor: "green",
+							borderWidth: 2,
+							data: numFoxesAfterEachTrial.map(({ total }) => total),
+							pointRadius: 0,
+						},
+					],
+					labels: numFoxesAfterEachTrial,
+				},
+				options: {
+					animation: { duration: 0 },
+					plugins: { legend: { display: false } },
+					scales: { x: { display: false }, y: { display: true } },
+				},
+				type: "line",
+			});
 			return () => {
+				lineChart.destroy();
 				scatterChart.destroy();
 			};
 		}
@@ -95,32 +147,102 @@ const LoktaVolterra: NextPage = () => {
 	const willFoxSurvive = (fox: Fox) =>
 		fox.numTrialsSurvivedSoFar < fox.lifespan;
 
-	const loktaExperimentDefinition: ExperimentDefinition<{
-		foxes: Array<Fox>;
-	}> = {
-		execute: (values) => {
-			return {
-				foxes: values.foxes.filter(willFoxSurvive).map((fox) => ({
-					...fox,
-					numTrialsSurvivedSoFar: fox.numTrialsSurvivedSoFar + 1,
-					point: takeAStep(fox.point, 0.05, {
-						max: { x: 1, y: 1 },
-						min: { x: 0, y: 0 },
-					}),
-				})),
-			};
-		},
-		initialValues: {
-			foxes: Array.from({ length: 10 }, () => ({
-				lifespan: Math.floor(MAXIMUM_LIFESPAN * (0.5 + Math.random() * 0.5)),
-				numTrialsSurvivedSoFar: 100,
-				point: { x: Math.random(), y: Math.random() },
-			})),
-		},
-		update: (values) => {
-			setFoxes(values.foxes);
-		},
-	};
+	const canFoxReproduce = (fox: Fox) =>
+		fox.numTrialsSurvivedSoFar > MIN_MATING_AGE &&
+		fox.numTrialsSinceLastReproduction >= MATING_RECOVERY_DURATION;
+
+	const numFoxesAfterEachTrialInternal: {
+		thatCanMate: number;
+		total: number;
+	}[] = [];
+
+	const loktaExperimentDefinition: ExperimentDefinition<{ foxes: Array<Fox> }> =
+		{
+			execute: (values) => {
+				numFoxesAfterEachTrialInternal.push({
+					thatCanMate: values.foxes.filter(canFoxReproduce).length,
+					total: values.foxes.length,
+				});
+
+				const fertileFoxes = values.foxes
+					.map((fox, index) => ({ fox, originalIndex: index }))
+					.filter(({ fox }) => canFoxReproduce(fox));
+				const fertileFoxTree = KDTree.from(
+					fertileFoxes.map(({ fox, originalIndex }) => [
+						{ fox, originalIndex },
+						Object.values(fox.point),
+					]),
+					2
+				);
+
+				const closestFertileNeighborOrigIndexByOrigIndex: Record<
+					number,
+					number
+				> =
+					fertileFoxes.length > 1
+						? Object.fromEntries(
+								fertileFoxes.map(({ fox, originalIndex }) => [
+									originalIndex,
+									fertileFoxTree.kNearestNeighbors(
+										2,
+										Object.values(fox.point)
+									)[1]?.originalIndex,
+								])
+						  )
+						: {};
+
+				const canPairMate = (foxA: Fox, foxB: Fox) =>
+					Math.hypot(foxA.point.x - foxB.point.x, foxA.point.y - foxB.point.y) <
+					MAX_MATING_DISTANCE;
+
+				const foxPairsWhoShouldMate = Object.entries(
+					closestFertileNeighborOrigIndexByOrigIndex
+				)
+					.map(([a, b]) => [Number(a), b])
+					.filter(
+						([indexA, indexB]) =>
+							indexA < indexB &&
+							closestFertileNeighborOrigIndexByOrigIndex[`${indexB}`] ===
+								indexA &&
+							canPairMate(values.foxes[indexA], values.foxes[indexB])
+					);
+
+				const foxesWhoMatedIndexes = foxPairsWhoShouldMate.flatMap((x) => x);
+
+				return {
+					foxes: values.foxes
+						.filter(willFoxSurvive)
+						.map((fox, index) => ({
+							...fox,
+							numTrialsSinceLastReproduction: foxesWhoMatedIndexes.includes(
+								index
+							)
+								? 0
+								: fox.numTrialsSinceLastReproduction + 1,
+							numTrialsSurvivedSoFar: fox.numTrialsSurvivedSoFar + 1,
+							point: takeAStep(fox.point, STEP_SIZE, {
+								max: { x: 1, y: 1 },
+								min: { x: 0, y: 0 },
+							}),
+						}))
+						.concat(
+							foxPairsWhoShouldMate.map(([originalIndexA, originalIndexB]) => {
+								const foxA = values.foxes[originalIndexA];
+								const foxB = values.foxes[originalIndexB];
+								return aFox({
+									x: (foxA.point.x + foxB.point.x) / 2,
+									y: (foxA.point.y + foxB.point.y) / 2,
+								});
+							})
+						),
+				};
+			},
+			initialValues: { foxes: Array.from({ length: INITIAL_NUM_FOXES }, aFox) },
+			update: (values) => {
+				setFoxes(values.foxes);
+				setNumFoxesAfterEachTrial(numFoxesAfterEachTrialInternal);
+			},
+		};
 
 	return (
 		<>
@@ -138,6 +260,10 @@ const LoktaVolterra: NextPage = () => {
 					<canvas
 						className="m-8 max-w-full max-h-[50vh] border-2"
 						ref={scatterChartRef}
+					/>
+					<canvas
+						className="m-8 max-w-full max-h-[50vh] border-2"
+						ref={lineChartRef}
 					/>
 				</Segment>
 			</div>
