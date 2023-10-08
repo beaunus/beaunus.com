b388dd49fae27e46d3cb9edb7ddd4726362ce75c
Beau Dobbin <beau@beaunus.com>
2023-10-08 12:06:05 +0900

    refactor: Segment className prop

--- components/Segment.tsx
+++ components/Segment.tsx
@@ -1,10 +1,13 @@
-import React, { FC, ReactNode } from "react";
+import { FC, HTMLAttributes, ReactNode } from "react";
 
-export const Segment: FC<{ children?: ReactNode; image?: ReactNode }> = ({
-	image,
-	children,
-}) => (
-	<section className="flex flex-col gap-6 justify-evenly items-center py-16 px-10 even:mt-2 w-full even:bg-gray-100">
+export const Segment: FC<{
+	children?: ReactNode;
+	className?: HTMLAttributes<HTMLElement>["className"];
+	image?: ReactNode;
+}> = ({ image, children, className = "" }) => (
+	<section
+		className={`flex flex-col gap-6 justify-evenly items-center py-16 px-10 even:mt-2 w-full even:bg-gray-100 ${className}`}
+	>
 		<div className="flex flex-col gap-5 items-center w-full md:flex-row md:gap-10 md:justify-center">
 			{image}
 			<div className="flex flex-col gap-5 items-center w-full max-w-2xl">
