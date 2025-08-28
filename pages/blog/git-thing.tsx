69d72cd9c6181d342a2018bcf827a8bc49e19aca
Beau Dobbin <beau@beaunus.com>
2025-08-28 16:24:24 +0900

    fix: path to outfile

--- pages/blog/git-thing.tsx
+++ pages/blog/git-thing.tsx
@@ -344,7 +344,7 @@ const GitThing: NextPage = () => {
 				</Typography>
 				<code>
 					git -C{" {path to git repository} "}log --numstat{" > "}
-					{" {path to outfile [prefix with organization/repo]}"}
+					{" {path to outfile [prefix with `organization:repo`]}"}
 				</code>
 				<Grid alignItems="center" container spacing={2}>
 					<Grid item xs={3}>
